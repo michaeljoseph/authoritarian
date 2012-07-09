882e815912aa7e3f94ddb71f9836cd2a403edb73
Michael Joseph <michaeljoseph+github@gmail.com>
2012-07-09 20:10:16 +0200

    Add api stubs

---
+++ authoritarian/api.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def _request():
+    pass
+
+
+def initialise(api_key):
+    pass
+
+
+def account_status():
+    pass
+
+
+def search(keyword, engine, locale, immediate=False):
+    pass
+
+
+def results(keyword, engine, locale, rank_date):
+    pass
